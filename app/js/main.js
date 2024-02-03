a76cd653eb584188ae17c49cfbb742182bb8840c
Serhii Shapovalenko <jackshkiper@gmail.com>
2024-02-03 16:59:36 +0200

    base burger js added

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,10 @@
-  
+// menu
+
+const menuBtn = document.querySelector('.menu__btn');
+const menuList = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+  document.body.classList.toggle('_lock');
+  menuList.classList.toggle('menu__list--active');
+  menuBtn.classList.toggle('menu__btn--close');
+});
